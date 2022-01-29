68ba3de12d3ae0bef0f850b5296d258ce862b8dc
Sergey Sovgut <sergey.sovgut@outlook.com>
2022-01-29 21:29:58 +0200

    Added settings window

--- component/settings/component.tsx
+++ component/settings/component.tsx
@@ -1,5 +1,39 @@
 import styles from './styles.module.css';
 
 export default function Settings() {
-	return <div className={styles.component}>Settings</div>;
+	return (
+		<div className={styles.component}>
+			<div className={styles.tabs}>
+				<div className={styles['settings-category-list']}>
+					<div className={styles['settings-category-list-item']}>
+						Graphics
+					</div>
+					<div className={styles['settings-category-list-item']}>
+						interface
+					</div>
+					<div className={styles['settings-category-list-item']}>
+						Game
+					</div>
+					<div className={styles['settings-category-list-item']}>
+						Control
+					</div>
+					<div className={styles['settings-category-list-item']}>
+						Audio
+					</div>
+					<div className={styles['settings-category-list-item']}>
+						Notifications
+					</div>
+					<div className={styles['settings-category-list-item']}>
+						Accessibility
+					</div>
+				</div>
+				<div className={styles['settings-game-controls']}>
+					<div className={styles['settings-game-controls-item']}>
+						Open lobby
+					</div>
+				</div>
+			</div>
+			<div className={styles['tab-content']}></div>
+		</div>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,34 @@ import { useEffect, useState } from 'react';
 import Commands from '~component/commands/component';
 import Constructions from '~component/constructions/component';
 import Minimap from '~component/minimap/component';
+import Settings from '~component/settings/component';
 import image from '~public/background.png';
 import styles from '~styles/Home.module.css';
 
 export default function Home() {
 	const [isScrollable, setScrollable] = useState(false);
+	const [isVisibleSettings, setSettingsVisibility] = useState(false);
 
 	useEffect(() => {
 		if (isScrollable) {
 			document.body.classList.toggle('scrollable');
 		}
+
+		document.body.addEventListener('keypress', onOpenSettingsKeyPress);
+
+		return () => {
+			document.body.removeEventListener(
+				'keypress',
+				onOpenSettingsKeyPress,
+			);
+		};
 	}, [isScrollable]);
+
+	function onOpenSettingsKeyPress(event) {
+		if (event.code === 'KeyO') {
+			setSettingsVisibility((prevState) => !prevState);
+		}
+	}
 
 	return (
 		<div className={styles.component}>
@@ -25,6 +42,7 @@ export default function Home() {
 				<Minimap />
 				<Constructions />
 				<Commands />
+				{isVisibleSettings && <Settings />}
 			</div>
 		</div>
 	);
